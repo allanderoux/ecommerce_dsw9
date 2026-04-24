c3d4e200cd6edd0c9929378ad85ca5626eb6f1bb
allanderoux <allanderoux@hotmail.com>
2026-04-24 00:16:54 +0000

    Feat: edicion de titulo

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send(`
-    Hello World - [REEMPLAZAR POR SU NOMBRE]
+    Hello World - Allan De Roux
     La aplicacion funciona en Render.
     Puerto: ${port} | Entorno: ${process.env.NODE_ENV || 'development'}
   `);
